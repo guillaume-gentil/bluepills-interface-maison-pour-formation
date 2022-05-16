4381265f092ee95d81d8751e477d50da77afb18c
guillaume-gentil <guillaumeg.dev@gmail.com>
2022-05-16 10:43:32 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+/* Includes DOCTYPE, HEAD-META, HEADER */
+require_once("./php/header.php");
+?>
+
+
+
+<?php 
+/* Includes FOOTER */
+require_once("./php/footer.php");
+?>
---
+++ php/header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="./assets/images/favicoGG_128.png" />
+    <!-- Titre de la page -->
+    <title>WebDev Galaxy - GG - [promo Einstein]</title>
+
+    <!-- Google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Macondo&family=Roboto&display=swap" rel="stylesheet">
+    <!-- Layout -->
+    <link rel="stylesheet" href="./assets/styles/layout.css">
+    <!-- Palette de couleur -->
+    <link rel="stylesheet" href="./assets/styles/colorDefault.css">
+    <!-- Feuilles de style -->
+    <link rel="stylesheet" href="./assets/styles/reset.css"> <!-- le reset -->
+    <link rel="stylesheet" href="./assets/styles/index.css"> <!-- mon style -->
+</head>
+
+<body>
+    <header>
+        <h1 class="titre-page">
+            <span class="titre-page__balise">DevWEB</span> 
+            <span class="titre-page__class">class</span>
+            <span class="titre-page__attr">einstein</span>
+        </h1>
+        <!-- Menu de navigation -->
+        <nav class="nav">
+            <a href="https://drive.google.com/drive/my-drive" class="nav__lien">
+                <img src="./assets/images/brand/google-drive48.png" alt="Drive" class="nav__lien__logo">
+            </a>
+            <a href="https://discord.com/channels/@me" class="nav__lien">
+                <img src="./assets/images/brand/discord360.png" alt="Discord" class="nav__lien__logo">
+            </a>
+            <a href="https://app.slack.com/client/T03BC2MMCRF/C03BP7E5F24" class="nav__lien">
+                <img src="./assets/images/brand/slack185.png" alt="Slack" class="nav__lien__logo">
+            </a>
+            <a href="https://github.com/guillaume-gentil"class="nav__lien">
+                <img src="./assets/images/brand/github630.png" alt="Github" class="nav__lien__logo">
+            </a>
+            <a href="https://cockpit.oclock.io/dashboard" class="nav__lien">
+                <img src="./assets/images/brand/oclock64.png" alt="Oclock" class="nav__lien__logo">
+            </a>
+            <a href="./einstein/" class="nav__lien">
+                Challenges
+            </a>
+            <a href="./html/bookmarks.html" class="nav__lien">
+                Bookmarks
+            </a>
+        </nav>
+    </header>
+
+    <main class="wrapper">
