1616beb1dd6c1bf74f155986a1f645f107ee0c58
guillaume-gentil <guillaumeg.dev@gmail.com>
2022-05-28 22:01:08 +0200

    Refonte de l'architecture et changement de présentation

--- index.php
+++ index.php
@@ -1,11 +1,154 @@
-<?php 
-/* Includes DOCTYPE, HEAD-META, HEADER */
-require_once("./php/header.php");
-?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="./img/avatar/favicoGG_128.png" />
 
+    <!-- Titre de la page -->
+    <title>WebDev Galaxy - GG - [promo Einstein]</title>
 
-<?php 
-/* Includes FOOTER */
-require_once("./php/footer.php");
-?>
+    <!-- Google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Macondo&family=Roboto&display=swap" rel="stylesheet">
+
+    <!-- Feuilles de style -->
+    <link rel="stylesheet" href="./css/reset.css"> <!-- le reset -->
+    <link rel="stylesheet" href="./css/theme/colorDefault.css"> <!-- le thème couleur -->
+    <link rel="stylesheet" href="./css/style.css"> <!-- mon style -->
+</head>
+
+<body>
+    <header>
+        <div class="galaxy"><!-- Menu de liens externes -->
+            <a href="https://drive.google.com/drive/my-drive" class="galaxy__link" title="Drive guillaumeg.dev">
+                <img src="./img/brand/google-drive48.png" alt="Drive" class="galaxy__logo">
+            </a>
+            <a href="https://start.me/p/aLg128/outils-dev" class="galaxy__link" title="Bookmarks">
+                <img src="./img/brand/startMe225.jpeg" alt="StartMe" class="galaxy__logo">
+            </a>
+            <a href="https://discord.com/channels/@me" class="galaxy__link" title="Discord O'clock">
+                <img src="./img/brand/discord360.png" alt="Discord" class="galaxy__logo">
+            </a>
+            <a href="https://app.slack.com/client/T03BC2MMCRF/C03BP7E5F24" class="galaxy__link" title="Slack O'clock">
+                <img src="./img/brand/slack185.png" alt="Slack" class="galaxy__logo">
+            </a>
+            <a href="https://cockpit.oclock.io/dashboard" class="galaxy__link" title="Cockpit O'clock">
+                <img src="./img/brand/oclock64.png" alt="Oclock" class="galaxy__logo">
+            </a>
+            <a href="https://github.com/guillaume-gentil"class="galaxy__link" title="GitHub guillaume-gentil">
+                <img src="./img/brand/github630.png" alt="Github" class="galaxy__logo">
+            </a>
+        </div><!-- Fin du menu de liens externes -->
+                    
+        <h1 class="titre-page"><!-- Titre de la bannière -->
+            <a href="http://localhost/einstein/"><span class="titre-page__balise">DevWEB</span><span class="titre-page__class">class</span><span class="titre-page__attr">einstein</span></a>
+        </h1><!-- Fin du titre de la bannière -->
+    </header>
+
+    <main class="flex">
+        
+        <nav class="categories"><!-- Menu de navigation des catégories, fixe sur la gauche -->
+            <ul class="categories__link">
+                <li><a href="#HTML">HTML</a></li>
+                <li><a href="#CSS">CSS</a></li>
+                <li><a href="#PHP">PHP</a></li>
+                <li><a href="#SYMPHONY">Symphony</a></li>                
+                <li><a href="#JS">JS</a></li>                
+                <li><a href="#REACT">React</a></li>    
+                <li><a href="#SQL">SQL</a></li>    
+            </ul>
+        </nav><!-- Fin du menu de navigation des catégories -->
+
+        <div class="main-content"><!-- Contenu des catégories -->
+
+            <section class="language"><!-- Section dédiée à HTML -->
+                <h2 id="HTML" class="language__title">HTML</h2>
+
+                <article class="language__notion">
+
+                </article>
+
+                <article class="language__notion">
+
+                </article>
+
+                <article class="language__notion">
+
+                </article>
+            </section><!-- Fin de section dédiée à HTML -->
+            
+            <section class="language"><!-- Section dédiée à CSS -->
+                <h2 id="CSS" class="language__title">CSS</h2>
+
+                <article class="language__notion">
+
+                </article>
+
+                <article class="language__notion">
+
+                </article>
+
+                <article class="language__notion">
+
+                </article>
+
+                <article class="language__notion">
+
+                </article>
+            </section><!-- Fin de section dédiée à CSS -->
+
+            <section class="language"><!-- Section dédiée à PHP -->
+                <h2 id="PHP" class="language__title">PHP</h2>
+
+                <article class="language__notion">
+
+                </article>
+            </section><!-- Fin de section dédiée à PHP -->
+
+            <section class="language"><!-- Section dédiée à Symphony -->
+                <h2 id="SYMPHONY" class="language__title">Symphony</h2>
+
+                <article class="language__notion">
+
+                </article>
+            </section><!-- Fin de section dédiée à Symphony -->
+
+            <section class="language"><!-- Section dédiée à JavaScript -->
+                <h2 id="JS" class="language__title">JS</h2>
+
+                <article class="language__notion">
+
+                </article>
+            </section><!-- Fin de section dédiée à Javascript -->
+
+            <section class="language"><!-- Section dédiée à React -->
+                <h2 id="REACT" class="language__title">React</h2>
+
+                <article class="language__notion">
+
+                </article>
+            </section><!-- Fin de section dédiée à React -->
+            
+            <section class="language"><!-- Section dédiée à SQL -->
+                <h2 id="SQL" class="language__title">SQL</h2>
+
+                <article class="language__notion">
+
+                </article>
+            </section><!-- Fin de section dédiée à SQL -->
+
+        </div><!-- Fin du contenu des catégories -->
+    </main>
+
+    <footer class="footer"><!-- Footer -->
+        <p>
+            Site réalisé par <a href="https://www.linkedin.com/in/ggentil/"><span class="footer__auteur">Guillaume Gentil</span></a><a href="http://localhost/einstein/"><span class="footer__promo">promo Einstein</span></a><span class="date">été 2022</span>
+        </p>
+    </footer>
+</body>
+</html>
